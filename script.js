584b09900011b4421d89317ff014fee7693d62e4
Benji <snowrepor@gmail.com>
2021-11-23 21:38:30 -0700

    colors for time in js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+var todoInput = document.querySelectorAll(".todo-text");
+var btn = document.querySelectorAll(".btn");
+//Load jQuery
 $(document).ready(function() {
 // Show Date and Time in Header 
 $('#currentDay').append();
 
 function addDate() {
-$('#currentDay').html(moment().format('MMMM Do YYYY, h:mm'));
+$('#currentDay').html(moment().format('MMMM Do YYYY, h:mm a'));
 } setInterval(addDate, 1000);
 
 
 });
+
