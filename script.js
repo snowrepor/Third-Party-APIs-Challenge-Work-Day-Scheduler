a595a2d9df8e7257e2584bfda360e99c7e0f569a
Benji <snowrepor@gmail.com>
2021-11-23 21:39:57 -0700

    html fix

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ $('#currentDay').html(moment().format('MMMM Do YYYY, h:mm a'));
 
 });
 
+// Changes color for given hours
+var repeater;
+
+function showcurrenttime()
+{
+    //$('.crnttym').text(moment().format('MMMM Do YYYY, h:mm a'));
+    $('.todoInput').each(function (i, el)
+    {
+        if ($(this).data('time') < moment().format('YYYYMMDDHHmm'))
+        {
+            $(this).parent().addClass('.past');
+        };
+        if ($(this).data('time') > moment().format('YYYYMMDDHHmm') && $(this).data('time') < moment().add('h',1).format('YYYYMMDDHHmm'))
+        {
+            $(this).parent().addClass('.present');
+        };
+    });
+    repeater = setTimeout(showcurrenttime);
+}
+showcurrenttime();
